5874d6e4bfe8a2fb9dfe8e9bf9f3ce789d97e046
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-07-10 23:59:59 +0530

    Updates for 2021-07-10 23:59:59

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+import json
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+
+    num = random.randint(0,10000)
+
+    print(f"Current random number is {num}")
+    if num%2 == 0:
+      print(f"current number is even")
+    else:
+      print(f"current number is odd")
+
+    return render_template('index.html',num=num)
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0",port=7000, debug=True)
